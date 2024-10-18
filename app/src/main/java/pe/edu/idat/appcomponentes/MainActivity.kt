e7a9d4061c2f73625de2b9e6a2c2611c2fbdaa10
Viccho2828 <kikekollizion@gmail.com>
2024-10-17 21:14:34 -0500

    mainActivity terminado

--- app/src/main/java/pe/edu/idat/appcomponentes/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/appcomponentes/MainActivity.kt
@@ -1,20 +1,28 @@
 package pe.edu.idat.appcomponentes
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pe.edu.idat.appcomponentes.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btningresar.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intentRegistro = Intent(this,
+            RegistroActivity::class.java)
+        startActivity(intentRegistro)
     }
 }
